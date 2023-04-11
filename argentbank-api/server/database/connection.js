c9580e943d979c6419d54b1e1bab4676b6845b90
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2023-04-11 21:14:11 +0200

    argent bank api

--- argentbank-api/server/database/connection.js
+++ argentbank-api/server/database/connection.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
 
+
+const mongoose = require('mongoose')
+// const databaseUrl =
+//   process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+const databaseUrl = "mongodb+srv://valentinmam:Kaujn0Vz9g3Vs4Ri@argentbank.hv4txfd.mongodb.net/?retryWrites=true&w=majority"
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    console.log("000000000000000000:"+databaseUrl)
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
