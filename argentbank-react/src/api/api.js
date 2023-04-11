94a6f1911df85d8a2c2f677f29699158d4b3c9dc
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2023-04-11 21:14:54 +0200

    api

---
+++ argentbank-react/src/api/api.js
@@ -0,0 +1,52 @@
+const signin = (email, password) => {
+  return fetch('http://localhost:3001/api/v1/user/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      return data.body.token
+      //return getProfileInfos(data.body.token)
+    })
+}
+
+const getProfileInfos = (token) => {
+  return fetch('http://localhost:3001/api/v1/user/profile', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      return data.body
+    })
+}
+
+const modifyProfile = (token, udpdatedFirstName, udpdatedLastName) => {
+  return fetch('http://localhost:3001/api/v1/user/profile', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      firstName: udpdatedFirstName,
+      lastName: udpdatedLastName,
+    }),
+  }).then((res) => {
+    return res.json()
+  })
+}
+export { getProfileInfos, modifyProfile, signin }
