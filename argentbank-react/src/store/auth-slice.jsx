f26f7e74390147e59f16852fdf008a23ef544a9d
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2023-05-17 22:18:02 +0200

    update redux for logout and futur loader

---
+++ argentbank-react/src/store/auth-slice.jsx
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialAuthState = {
+  isAuthenticated: false,
+  token: null,
+  firstName: null,
+  lastName: null,
+}
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    setToken(state, action) {
+      state.token = action.payload
+      state.isAuthenticated = true
+    },
+    login(state, action) {
+      state.isAuthenticated = true
+      state.firstName = action.payload.firstName
+      state.lastName = action.payload.lastName
+    },
+    updateName(state, action) {
+      state.firstName = action.payload.firstName
+      state.lastName = action.payload.lastName
+    },
+    logout(state) {
+      state.isAuthenticated = false
+      state.firstName = null
+      state.lastName = null
+      state.token = null
+    },
+  },
+})
+
+export const authActions = authSlice.actions
+
+export default authSlice.reducer
