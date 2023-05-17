0aa724fd930150fcbfae8039c2383476a1f55638
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2023-05-17 22:21:13 +0200

    rename pages and js to jsx files

---
+++ argentbank-react/src/pages/Login.jsx
@@ -0,0 +1,90 @@
+/* eslint-disable id-length */
+import { useState } from 'react'
+import { authActions } from '../store/auth-slice'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const login = (e) => {
+    e.preventDefault()
+
+    fetch('http://localhost:3001/api/v1/user/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => {
+        return res.json()
+      })
+      .then((response) => {
+        if (response.body.token) {
+          //set token in redux store
+          dispatch(authActions.setToken(response.body.token))
+          //get user profile data from API
+          fetch('http://localhost:3001/api/v1/user/profile', {
+            method: 'POST',
+            headers: {
+              Authorization: `Bearer ${response.body.token}`,
+            },
+          })
+            .then((res) => {
+              return res.json()
+            })
+            .then((response) => {
+              //set redux store user infos
+              dispatch(authActions.login(response.body))
+            })
+          //redirect to profile page
+          navigate('/profile')
+        }
+      })
+      .catch((err) => {
+        return console.log(err)
+      })
+  }
+
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form onSubmit={login}>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              onChange={(e) => {
+                return setEmail(e.target.value)
+              }}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => {
+                return setPassword(e.target.value)
+              }}
+            />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+
+          <button className="sign-in-button">Sign In</button>
+        </form>
+      </section>
+    </main>
+  )
+}
+
+export default Login
---
+++ argentbank-react/src/pages/Profile.jsx
@@ -0,0 +1,145 @@
+/* eslint-disable id-length */
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { authActions } from '../store/auth-slice'
+
+const Profile = () => {
+  const [isEditingName, setIsEditingName] = useState(false)
+  const [newFirstName, setNewFirstName] = useState()
+  const [newLastName, setNewLastName] = useState()
+  const dispatch = useDispatch()
+  const firstName = useSelector((state) => {
+    return state.authReducer.firstName
+  })
+  const lastName = useSelector((state) => {
+    return state.authReducer.lastName
+  })
+  const token = useSelector((state) => {
+    return state.authReducer.token
+  })
+
+  const saveNewName = (e) => {
+    e.preventDefault()
+
+    fetch('http://localhost:3001/api/v1/user/profile', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        firstName: newFirstName,
+        lastName: newLastName,
+      }),
+    })
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        console.log(data)
+        dispatch(authActions.updateName(data.body))
+        setIsEditingName(false)
+      })
+      .catch((err) => {
+        return console.log(err)
+      })
+  }
+
+  return (
+    <main className="main bg-dark">
+      <div className="header">
+        {!isEditingName && (
+          <div>
+            <h1>
+              Welcome back
+              <br />
+              {firstName} {lastName}
+            </h1>
+            <button
+              className="edit-button"
+              onClick={() => {
+                return setIsEditingName(true)
+              }}
+            >
+              Edit Name
+            </button>
+          </div>
+        )}
+        {isEditingName && (
+          <div>
+            <h1>
+              Welcome back
+              <br />
+            </h1>
+            <div className="name-edit-container">
+              <input
+                className="edit-input"
+                type="firstname"
+                id="firstname"
+                placeholder={firstName}
+                onChange={(e) => {
+                  setNewFirstName(e.target.value)
+                }}
+              />
+              <input
+                className="edit-input"
+                type="lastname"
+                id="lastname"
+                placeholder={lastName}
+                onChange={(e) => {
+                  setNewLastName(e.target.value)
+                }}
+              />
+            </div>
+            <div className="edit-buttons-container">
+              <button className="edit-name-button" onClick={saveNewName}>
+                Save
+              </button>
+              <button
+                className="edit-name-button"
+                onClick={() => {
+                  return setIsEditingName(false)
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+      <h2 className="sr-only">Accounts</h2>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+          <p className="account-amount">$2,082.79</p>
+          <p className="account-amount-description">Available Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+          <p className="account-amount">$10,928.42</p>
+          <p className="account-amount-description">Available Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+          <p className="account-amount">$184.30</p>
+          <p className="account-amount-description">Current Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default Profile
