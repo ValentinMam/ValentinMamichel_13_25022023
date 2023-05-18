6c828f37d4f1d043713e8baf90687c84f0ede4d3
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2023-05-18 21:34:48 +0200

    login error ok

--- argentbank-react/src/pages/Login.jsx
+++ argentbank-react/src/pages/Login.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom'
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
+  const [invalidUser, setInvalidUser] = useState('')
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
@@ -44,12 +44,48 @@ const Login = () => {
           navigate('/profile')
         }
       })
-      .catch((err) => {
-        return console.log(err)
+      // eslint-disable-next-line no-unused-vars
+      .catch((error) => {
+        return setInvalidUser('Utilisateur ou mot de passe incorrect')
       })
   }
 
-  return (
+  return invalidUser ? (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form onSubmit={login}>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              onChange={(e) => {
+                return setEmail(e.target.value)
+              }}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => {
+                return setPassword(e.target.value)
+              }}
+            />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <p className="invalid-message">{invalidUser}</p>
+          <button className="sign-in-button">Sign In</button>
+        </form>
+      </section>
+    </main>
+  ) : (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
